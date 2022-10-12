82ebd405aaf2d799d91713109cf4810e226a0e74
devlinzhou <devlinzhou@tencent.com>
2022-10-12 15:38:52 +0800

    modify

--- UnrealDemo/Source/UnrealDemo/GUnrealUtility.cpp
+++ UnrealDemo/Source/UnrealDemo/GUnrealUtility.cpp
@@ -1,7 +1,5 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#pragma once
-
 #include "GUnrealUtility.h"
 #include "Kismet/KismetMathLibrary.h"
 #include "Kismet/KismetSystemLibrary.h"
