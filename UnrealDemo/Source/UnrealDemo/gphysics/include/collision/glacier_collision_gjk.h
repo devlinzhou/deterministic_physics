8d0eecf60a5563209056f55bb154dfb78241a64a
devlinzhou <devlinzhou@tencent.com>
2022-10-16 22:41:42 +0800

    modify

--- UnrealDemo/Source/UnrealDemo/gphysics/include/collision/glacier_collision_gjk.h
+++ UnrealDemo/Source/UnrealDemo/gphysics/include/collision/glacier_collision_gjk.h
@@ -31,6 +31,8 @@ class GCollision_GJK
 {
 public:
 
+    static const f32 s_DefaultEpsilon = GMath::Makef32(0,1,1000);
+
     static bool GJKTest( 
         const GShapeConvexBase& ShapA,
         const GTransform_QT&    TransformA,
@@ -39,7 +41,84 @@ public:
         IGlacierDraw*           pDebugDraw = nullptr );
 
 
+    template <typename ShapTypeA, typename ShapTypeB>
+    static inline GVector3 s_GetSupportPos(
+        const GVector3& DirectionA,
+        const ShapTypeA& ShapA,
+        const ShapTypeB& ShapB,
+        const GTransform_QT& LocalBToLocalA,
+        const GTransform_QT& LocalAToLocalB,
+        const GTransform_QT& TransformShapA,
+        const GTransform_QT& TransformShapB,
+        IGlacierDraw* pDebugDraw,
+        uint32_t                uColor)
+    {
+        GVector3 DirectionB = LocalAToLocalB.TransformNormal(-DirectionA);
+        GVector3 PosA = ShapA.GetSupportLocalPos(DirectionA);
+        GVector3 PosB = ShapB.GetSupportLocalPos(DirectionB);
 
+        if (pDebugDraw != nullptr)
+        {
+
+            f32 ff = GMath::FromFloat(0.02f);
+            f32 f2 = GMath::FromFloat(0.04f);
+
+            pDebugDraw->DrawSphere(GTransform_QT(GQuaternion::Identity(), TransformShapA.TransformPosition(PosA)), ff, uColor, 12);
+            pDebugDraw->DrawSphere(GTransform_QT(GQuaternion::Identity(), TransformShapB.TransformPosition(PosB)), ff, uColor, 12);
+            pDebugDraw->DrawLine(TransformShapA.m_Translation, TransformShapA.m_Translation + TransformShapA.TransformNormal(DirectionA) * f2, uColor);
+            pDebugDraw->DrawLine(TransformShapB.m_Translation, TransformShapB.m_Translation + TransformShapB.TransformNormal(DirectionB) * f2, uColor);
+        }
+
+        return PosA - LocalBToLocalA.TransformPosition(PosB);
+    }
+
+    template <typename ShapTypeA, typename ShapTypeB>
+    static inline f32 GJKDistance(
+        const ShapTypeA& ShapA, const GTransform_QT& TransformA,
+        const ShapTypeB& ShapB, const GTransform_QT& TransformB,
+        f32 Epsilon = s_DefaultEpsilon,
+        IGlacierDraw* pDebugDraw = nullptr )
+    {
+        GTransform_QT WorlToLocal_A = TransformA.GetInverse();
+        GTransform_QT LocalBToLocalA = TransformB * WorlToLocal_A;
+        GTransform_QT LocalAToLocalB = LocalBToLocalA.GetInverse();
+
+
+        GVector3 DirectionA = LocalBToLocalA.TransformPosition(GVector3::Zero()).GetNormalize();
+        GVector3 SupportPos0 = s_GetSupportPos(DirectionA, ShapA, ShapB, LocalBToLocalA, LocalAToLocalB, TransformA, TransformB, pDebugDraw, 0x00FF0000);
+
+        f32 fDistance = SupportPos0.Size();
+        if (f < GMath::Epsilon())
+        {
+            return fDistance;
+        }
+
+        while ( fDistance > Epsilon )
+        {
+            f32 fDotTest = GVector3::DotProduct( SupportPos0 );
+            if (fDotTest > GMath::Zero())
+            {
+                return fDotTest; // not accurate
+            }
+
+
+
+
+
+
+
+            f32 fDistance = SupportPos0.Size();
+            if (f < GMath::Epsilon())
+            {
+                return fDistance;
+            }
+        }
+
+
+
+        return fDistance;
+    
+    }
 
 };
 
